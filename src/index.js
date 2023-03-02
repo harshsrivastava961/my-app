ff9a182826fcc5238a67effb3d0d0c0d8cb8347e
harshsrivastava961 <harshsrivastava961@gmail.com>
2023-03-02 14:44:25 +0530

    changes made

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App url="https://chair-web-ar.glitch.me" />
+    <App url="https://64006829e10df8000895e5bb--extraordinary-basbousa-2b58a1.netlify.app" />
   </React.StrictMode>
 );
 
