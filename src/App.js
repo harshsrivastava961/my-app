4e566ca777651a3094411b750165842d35bc16ca
harshsrivastava961 <harshsrivastava961@gmail.com>
2023-03-02 14:57:52 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -37,9 +37,11 @@ function App({url}) {
         src="https://cdn.glitch.global/612f90f6-8704-4969-8e31-17d3069b0154/arm_chair__furniture%20(1).glb?v=1676215192645"
         ios-src="https://cdn.glitch.global/612f90f6-8704-4969-8e31-17d3069b0154/Arm_chair__Furniture.usdz?v=1676215324669"
         shadow-intensity="1"
+        ar={true}
+        auto-rotate={true}
         camera-controls
         style={{ width: "100%", height: "500px" }} 
-        auto-rotate ar>
+         >
       </model-viewer>
       <div  ><img style={{ width: "200px", height: "200px" }} src={src} alt = "hello"/></div>
       
